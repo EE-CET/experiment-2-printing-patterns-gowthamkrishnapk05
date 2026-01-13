de905e9881f1ff4dcbe815d716e74cbcd2a0254f
Gowtham Krishna P K <gowthamkrishnapk05@gmail.com>
2026-01-13 21:44:58 +0530

    updated

--- src/PatternPrinting.java
+++ src/PatternPrinting.java
@@ -10,7 +10,7 @@ public class PatternPrinting {
             System.out.print("* ");
         }
         System.out.println("");
-        for(int i=0;i<n;i++)
+        for(int i=1;i<n;i++)
         {
             System.out.println("*");
         }
