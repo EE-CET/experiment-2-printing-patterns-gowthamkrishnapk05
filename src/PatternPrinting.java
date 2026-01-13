f741f3309fbe8c5fa078c86950584047f7fba77b
Gowtham Krishna P K <gowthamkrishnapk05@gmail.com>
2026-01-13 21:39:24 +0530

    printed the pattern

--- src/PatternPrinting.java
+++ src/PatternPrinting.java
@@ -1,5 +1,19 @@
 import java.util.Scanner;
 
 public class PatternPrinting {
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=0;i<n;i++)
+        {
+            System.out.print("* ");
+        }
+        System.out.println("");
+        for(int i=0;i<n;i++)
+        {
+            System.out.println("*");
+        }
+    }
     
 }
